5b4625d5f6a099877594b20f4e00f67f93763ef2
xel <65361679+intrusions@users.noreply.github.com>
2023-06-05 17:52:55 +0200

    Delete Mutex.hpp

--- Mutex.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <atomic>
-
-struct Mutex {
-	std::atomic<bool> trigIsActive { false };
-	std::atomic<bool> radarIsActive { false };
-	std::atomic<bool> aimbotIsActive { false };
-	std::atomic<bool> glowIsActive { false };
-};
